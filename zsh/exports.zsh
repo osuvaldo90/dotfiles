f5cae01547f45fb790331320303b01299b46e7c2
Osvi Ramos <osuvaldo.ramos@gmail.com>
2026-02-19 11:54:45 -0500

    set minimal spaceship prompt

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,4 +1,4 @@
 # spaceship prompt
-#SPACESHIP_PROMPT_ORDER=(user dir host git node kubectl jobs exec_time line_sep exit_code char)
+SPACESHIP_PROMPT_ORDER=(user dir host git exec_time line_sep exit_code char)
 
 ZSH_AUTOSUGGEST_STRATEGY=(match_prev_cmd completion history)
