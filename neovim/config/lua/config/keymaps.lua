7f4b5784a80104e9dab3b72fbc66819a42945d4b
Osvi Ramos <osvi.ramos@vanta.com>
2026-03-11 20:58:58 +0000

    nvim: add keymap to copy buffer file relative path

--- neovim/config/lua/config/keymaps.lua
+++ neovim/config/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<leader>bc", function()
+  local path = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":.")
+  vim.fn.setreg("+", path)
+  vim.notify("Copied: " .. path)
+end, { desc = "Copy buffer relative path" })
