83756f5b5419f0a2569bd82e51447e7e419debcb
Osvi Ramos <osuvaldo.ramos@gmail.com>
2026-03-11 13:53:12 -0400

    zsh: use nvim as default editor

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -2,3 +2,6 @@
 SPACESHIP_PROMPT_ORDER=(user dir host git exec_time line_sep exit_code char)
 
 ZSH_AUTOSUGGEST_STRATEGY=(match_prev_cmd completion history)
+
+export EDITOR=nvim
+export VISUAL=nvim
