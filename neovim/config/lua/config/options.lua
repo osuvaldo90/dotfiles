b32ad132c96f694ac668c0c249f3c34b2d0a1d13
Osvi Ramos <osvi.ramos@vanta.com>
2026-03-14 20:33:49 +0000

    nvim: show eslint etc. diagnostics below code line

--- neovim/config/lua/config/options.lua
+++ neovim/config/lua/config/options.lua
@@ -2,6 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Show diagnostics on their own line below the code instead of inline
+vim.diagnostic.config({ virtual_lines = true, virtual_text = false })
+
 -- Over SSH (including inside tmux), LazyVim disables clipboard. Override with
 -- the built-in OSC 52 provider so yanks reach the macOS clipboard via the
 -- terminal emulator.
