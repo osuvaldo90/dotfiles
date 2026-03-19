c4992416c7184f6676f5a92db933447895fb2fa3
Osvi Ramos <osvi.ramos@vanta.com>
2026-03-19 15:40:49 +0000

    nvim: add sattelite scrollbar plugin

---
+++ neovim/config/lua/plugins/scrollbar.lua
@@ -0,0 +1,14 @@
+return {
+  "lewis6991/satellite.nvim",
+  opts = {
+    current_only = false,
+    winblend = 50,
+    handlers = {
+      cursor = { enable = true },
+      diagnostic = { enable = true },
+      gitsigns = { enable = true },
+      marks = { enable = false },
+      quickfix = { enable = false },
+    },
+  },
+}
